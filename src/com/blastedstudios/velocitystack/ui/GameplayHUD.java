3252a9772ca40e9301f467e286079be2acf17876
narfman0 <narfman0@gmail.com>
2014-07-03 12:06:52 -0400

    -Alpha out HUD elements after a time period

--- src/com/blastedstudios/velocitystack/ui/GameplayHUD.java
+++ src/com/blastedstudios/velocitystack/ui/GameplayHUD.java
@@ -1,23 +1,31 @@
 package com.blastedstudios.velocitystack.ui;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.blastedstudios.gdxworld.util.Properties;
 
 public class GameplayHUD {
+	private static final long HUD_UP_TIME = Properties.getLong("money.hud.up.time", 2000),
+			HUD_FADE_TIME = Properties.getLong("money.hud.fade.time", 2000);
 	private final Label cashLabel;
 	private final GameplayScreen screen;
+	private final Window bankWindow, currentWindow;
+	private long lastCash, timeLastCash;
 	
 	public GameplayHUD(GameplayScreen screen){
 		this.screen = screen;
-		Window bankWindow = new Window("Bank", screen.getSkin());
+		bankWindow = new Window("Bank", screen.getSkin());
 		bankWindow.add(new Label(screen.getBank()+"$", screen.getSkin()));
 		bankWindow.pack();
 		bankWindow.setY(Gdx.graphics.getHeight() - bankWindow.getHeight());
 		screen.getStage().addActor(bankWindow);
 		
-		Window currentWindow = new Window("Current", screen.getSkin());
+		currentWindow = new Window("Current", screen.getSkin());
 		currentWindow.add(cashLabel = new Label("---------$", screen.getSkin()));
+		cashLabel.setText((lastCash = screen.getCash()) + "$");
 		currentWindow.pack();
 		currentWindow.setY(Gdx.graphics.getHeight() - currentWindow.getHeight() - bankWindow.getHeight());
 		screen.getStage().addActor(currentWindow);
@@ -25,9 +33,28 @@ public class GameplayHUD {
 		float width = Math.max(currentWindow.getWidth(), bankWindow.getWidth());
 		currentWindow.setWidth(width);
 		bankWindow.setWidth(width);
+		timeLastCash = System.currentTimeMillis();
 	}
 	
 	public void render(float dt){
-		cashLabel.setText(" " + screen.getCash()+"$");
+		if(lastCash != screen.getCash()){
+			cashLabel.setText((lastCash = screen.getCash()) + "$");
+			timeLastCash = System.currentTimeMillis();
+		}
+		
+		Color newColor = bankWindow.getColor();
+		newColor.a = 1f;
+		if(System.currentTimeMillis() - timeLastCash < HUD_UP_TIME)
+			newColor.a = 1f;
+		else{
+			long timePastHUDUp = System.currentTimeMillis() - timeLastCash - HUD_UP_TIME;
+			if(timePastHUDUp > HUD_FADE_TIME)
+				newColor.a = 0f;
+			else
+				newColor.a = MathUtils.lerp(1f, 0f, ((float)timePastHUDUp)/((float)HUD_FADE_TIME));
+		}
+		bankWindow.setColor(newColor);
+		currentWindow.setColor(newColor);
+			
 	}
 }
